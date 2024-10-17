26a707cf8063747fa1e86236334c9e4089f1237a
Edu-Guerrero <144632957+Edu-Guerrero@users.noreply.github.com>
2024-10-17 09:44:03 -0500

    Pico Solutions Edu

---
+++ PicoCTF_Edu/decrypt_C3.py
@@ -0,0 +1,12 @@
+chars = ""
+from fileinput import input
+for line in input():
+    chars += line
+b = 1
+
+for i in range(len(chars)):
+    if i == (b * b * b):
+        print(chars[i], end="")
+        b += 1
+
+print()
